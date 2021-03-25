7b427691d1221dc0dbc1ce3ed2d915ac64dffec1
HieuNguyen0908 <togetherforeverwithyou2000@gmail.com>
2021-03-25 17:18:16 +0700

    Add frm_Maim

---
+++ PetShop/PetShop/frm_Main.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PetShop
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuTepTin = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuThoat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDanhMuc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHangHoa = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNhanHieu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuNhanVien = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKhachHang = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHoaDon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuTimKiem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuGioiThieu = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuTepTin,
+            this.mnuDanhMuc,
+            this.mnuHoaDon,
+            this.mnuTimKiem,
+            this.mnuGioiThieu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(846, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuTepTin
+            // 
+            this.mnuTepTin.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuThoat});
+            this.mnuTepTin.Name = "mnuTepTin";
+            this.mnuTepTin.Size = new System.Drawing.Size(69, 24);
+            this.mnuTepTin.Text = "Tệp tin";
+            this.mnuTepTin.Click += new System.EventHandler(this.mnuTepTin_Click);
+            // 
+            // mnuThoat
+            // 
+            this.mnuThoat.Name = "mnuThoat";
+            this.mnuThoat.Size = new System.Drawing.Size(224, 26);
+            this.mnuThoat.Text = "Thoát";
+            // 
+            // mnuDanhMuc
+            // 
+            this.mnuDanhMuc.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuHangHoa,
+            this.mnuNhanHieu,
+            this.toolStripSeparator1,
+            this.mnuNhanVien,
+            this.mnuKhachHang});
+            this.mnuDanhMuc.Name = "mnuDanhMuc";
+            this.mnuDanhMuc.Size = new System.Drawing.Size(90, 24);
+            this.mnuDanhMuc.Text = "Danh mục";
+            // 
+            // mnuHangHoa
+            // 
+            this.mnuHangHoa.Name = "mnuHangHoa";
+            this.mnuHangHoa.Size = new System.Drawing.Size(224, 26);
+            this.mnuHangHoa.Text = "Thức Ăn";
+            this.mnuHangHoa.Click += new System.EventHandler(this.mnuHangHoa_Click);
+            // 
+            // mnuNhanHieu
+            // 
+            this.mnuNhanHieu.Name = "mnuNhanHieu";
+            this.mnuNhanHieu.Size = new System.Drawing.Size(224, 26);
+            this.mnuNhanHieu.Text = "Nhãn hiệu";
+            this.mnuNhanHieu.Click += new System.EventHandler(this.mnuNhanHieu_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // mnuNhanVien
+            // 
+            this.mnuNhanVien.Name = "mnuNhanVien";
+            this.mnuNhanVien.Size = new System.Drawing.Size(224, 26);
+            this.mnuNhanVien.Text = "Nhân viên";
+            this.mnuNhanVien.Click += new System.EventHandler(this.mnuNhanVien_Click);
+            // 
+            // mnuKhachHang
+            // 
+            this.mnuKhachHang.Name = "mnuKhachHang";
+            this.mnuKhachHang.Size = new System.Drawing.Size(224, 26);
+            this.mnuKhachHang.Text = "Khách hàng";
+            this.mnuKhachHang.Click += new System.EventHandler(this.mnuKhachHang_Click);
+            // 
+            // mnuHoaDon
+            // 
+            this.mnuHoaDon.Name = "mnuHoaDon";
+            this.mnuHoaDon.Size = new System.Drawing.Size(81, 24);
+            this.mnuHoaDon.Text = "Hóa đơn";
+            this.mnuHoaDon.Click += new System.EventHandler(this.mnuHoaDon_Click);
+            // 
+            // mnuTimKiem
+            // 
+            this.mnuTimKiem.Name = "mnuTimKiem";
+            this.mnuTimKiem.Size = new System.Drawing.Size(84, 24);
+            this.mnuTimKiem.Text = "Tìm kiếm";
+            this.mnuTimKiem.Click += new System.EventHandler(this.mnuTimKiem_Click);
+            // 
+            // mnuGioiThieu
+            // 
+            this.mnuGioiThieu.Name = "mnuGioiThieu";
+            this.mnuGioiThieu.Size = new System.Drawing.Size(87, 24);
+            this.mnuGioiThieu.Text = "Giới thiệu";
+            this.mnuGioiThieu.Click += new System.EventHandler(this.mnuGioiThieu_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(846, 426);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 459);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frm_Main";
+            this.Text = "Quản Lý Của Hàng Bán Thức Ăn Thú Cưng";
+            this.Load += new System.EventHandler(this.frm_Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuTepTin;
+        private System.Windows.Forms.ToolStripMenuItem mnuThoat;
+        private System.Windows.Forms.ToolStripMenuItem mnuDanhMuc;
+        private System.Windows.Forms.ToolStripMenuItem mnuHangHoa;
+        private System.Windows.Forms.ToolStripMenuItem mnuNhanHieu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuNhanVien;
+        private System.Windows.Forms.ToolStripMenuItem mnuKhachHang;
+        private System.Windows.Forms.ToolStripMenuItem mnuHoaDon;
+        private System.Windows.Forms.ToolStripMenuItem mnuTimKiem;
+        private System.Windows.Forms.ToolStripMenuItem mnuGioiThieu;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
